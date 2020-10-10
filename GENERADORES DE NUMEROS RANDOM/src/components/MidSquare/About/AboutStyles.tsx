ecd58a59c409c99a3aa24a737e416d67176ab684
gdaguilarc <gd.aguilarc@gmail.com>
2020-10-09 20:45:42 -0500

    [UPDATE] Some Refactoring

---
+++ GENERADORES DE NUMEROS RANDOM/src/components/MidSquare/About/AboutStyles.tsx
@@ -0,0 +1,31 @@
+import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    text: {
+      fontFamily: "Montserrat-Bold",
+    },
+    header: {
+      fontWeight: "bold",
+    },
+    media: {
+      height: 0,
+      paddingTop: "56.25%",
+    },
+    cards: {
+      padding: theme.spacing(4),
+      color: theme.palette.text.secondary,
+      borderRadius: "0.3px",
+      border: "3px solid #7c8599",
+    },
+    section2: {
+      margin: theme.spacing(2, 0),
+    },
+    imageCentered: {
+      alignItems: "center",
+      textAlign: "center",
+    },
+  })
+);
+
+export default useStyles;
---
+++ GENERADORES DE NUMEROS RANDOM/src/components/MidSquare/History/index.tsx
@@ -0,0 +1,4 @@
+import HistoryList from "./HistoryList";
+import HistoryRow from "./HistoryRow";
+
+export { HistoryList, HistoryRow };
